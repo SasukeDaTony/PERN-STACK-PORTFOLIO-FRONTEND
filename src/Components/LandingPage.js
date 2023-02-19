581bf5a29864c53145fa84e945e7baba2db6d33f
SasukeDaTony <tonytaylor@pursuit.org>
2023-02-19 14:40:38 -0500

    btn

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -13,7 +13,7 @@ export default function LandingPage() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Sonne_Meer_und_M%C3%B6we.jpg/400px-Sonne_Meer_und_M%C3%B6we.jpg"
         alt="logo"
       /><br/>
-      <button className="button" onClick={() => navigate("/treatments")}>Go To All Treatments</button>
+      <button className="btn" onClick={() => navigate("/treatments")}>Go To All Treatments</button>
     </div>
   );
 }
